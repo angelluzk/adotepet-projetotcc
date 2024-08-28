057cc2d74c7dd2defbdc4a83532651c511186021
Angelita da Luz Mota <gamepandinha@gmail.com>
2024-08-28 13:15:19 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); //Inicia a sessão
-
-//Destruir a sessão
-$_SESSION = array();
-session_destroy();
-header("Location: index.php"); //Redirecionar para a página index
-exit();
-?>
