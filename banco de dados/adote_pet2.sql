bcd972a2f2b65ec6d9154f0b020e398ad578e6a3
jcleston <janes.cleston.silva@gmail.com>
2024-08-30 22:38:12 -0300

    feat: atualização da estrutura do banco de dados

--- banco de dados/adote_pet2.sql
+++ banco de dados/adote_pet2.sql
@@ -14,17 +14,107 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Exportação de dados foi desmarcado.
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura do banco de dados para adote_pet
+CREATE DATABASE IF NOT EXISTS `adote_pet` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `adote_pet`;
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela adote_pet.doacoes
+CREATE TABLE IF NOT EXISTS `doacoes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pet_id` int DEFAULT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `pet_id` (`pet_id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `doacoes_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`),
+  CONSTRAINT `doacoes_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela adote_pet.doacoes: ~0 rows (aproximadamente)
 
--- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela adote_pet.especie
+CREATE TABLE IF NOT EXISTS `especie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela adote_pet.especie: ~0 rows (aproximadamente)
+INSERT INTO `especie` (`id`, `nome`) VALUES
+	(1, 'Cachorro'),
+	(2, 'Gato');
+
+-- Copiando estrutura para tabela adote_pet.fotos
+CREATE TABLE IF NOT EXISTS `fotos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `pet_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fotos_pets_FK` (`pet_id`),
+  CONSTRAINT `fotos_pets_FK` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela adote_pet.fotos: ~0 rows (aproximadamente)
+INSERT INTO `fotos` (`id`, `nome`, `url`, `pet_id`) VALUES
+	(1, 'teste', 'local da foto', 1);
+
+-- Copiando estrutura para tabela adote_pet.perfis
+CREATE TABLE IF NOT EXISTS `perfis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela adote_pet.perfis: ~0 rows (aproximadamente)
+INSERT INTO `perfis` (`id`, `nome`) VALUES
+	(1, 'Funcionário'),
+	(2, 'Usuário');
+
+-- Copiando estrutura para tabela adote_pet.pets
+CREATE TABLE IF NOT EXISTS `pets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `especie_id` int NOT NULL,
+  `raca` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cor` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idade` int NOT NULL,
+  `descricao` text COLLATE utf8mb4_unicode_ci,
+  `foto` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `criado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `pets_especie_FK` (`especie_id`),
+  CONSTRAINT `pets_especie_FK` FOREIGN KEY (`especie_id`) REFERENCES `especie` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela adote_pet.pets: ~0 rows (aproximadamente)
+INSERT INTO `pets` (`id`, `especie_id`, `raca`, `cor`, `idade`, `descricao`, `foto`, `criado_em`) VALUES
+	(1, 1, 'asd', 'asd', 1, 'asd', 'asd', '2024-08-30 23:16:15');
+
+-- Copiando estrutura para tabela adote_pet.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sobrenome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cpf` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `perfil_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `email` (`email`),
+  KEY `usuarios_perfis_FK` (`perfil_id`),
+  CONSTRAINT `usuarios_perfis_FK` FOREIGN KEY (`perfil_id`) REFERENCES `perfis` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela adote_pet.usuarios: ~2 rows (aproximadamente)
+INSERT INTO `usuarios` (`id`, `nome`, `sobrenome`, `cpf`, `telefone`, `email`, `senha`, `criado_em`, `perfil_id`) VALUES
+	(1, 'Angel', 'da Luz', '00000000000', '61951123456', 'angelluz@gmail.com', '$2y$10$XxVoalGyWrgsQpku7Iab5uaE0xMMZVvQWsRPfapOC5XrI9t32KWGm', '2024-08-28 13:53:17', 1),
+	(2, 'Lucas', 'Santos', '11111111111', '61985456789', 'lucas@gmail.com', '$2y$10$xQUhU8r7qpK23OIE0.EjUO.ZHzSYCyH7hhXD/twBpfM5IUsPTm4Fu', '2024-08-28 14:19:49', 2);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
